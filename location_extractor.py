e5aa7c683b9eb3f3b8a02bfa78ebc0bc447bcfff
Tulio Wetler <tuliowetler@gmail.com>
2025-06-14 01:23:14 +0100

    Base extractor idea

--- location_extractor.py
+++ location_extractor.py
@@ -1,8 +1,16 @@
 import os
-import requests
+
 from dotenv import load_dotenv
+from src.utils import load_config_yaml
 
 
 # Get environment variables
 load_dotenv()
 FOURSQUARE_KEY = os.getenv("FOURSQUARE_API_KEY")
+
+# Get config
+config = load_config_yaml("config.yaml")
+
+
+
+
